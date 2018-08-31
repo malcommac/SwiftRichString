f9882cb3ca347e5a193478c3ff14b024ab15aaa1
Daniele Margutti <hello@danielemargutti.com>
2018-08-31 14:38:54 +0200

    #40 Fixed compactMap for Swift 4.0

--- Sources/SwiftRichString/Extensions/UIKit+Extras.swift
+++ Sources/SwiftRichString/Extensions/UIKit+Extras.swift
@@ -163,3 +163,14 @@ extension UITextView {
 }
 
 #endif
+
+//MARK: - compactMap for Swift 4.0 (not necessary > 4.0)
+
+#if swift(>=4.1)
+#else
+extension Collection {
+	func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+		return try flatMap(transform)
+	}
+}
+#endif
