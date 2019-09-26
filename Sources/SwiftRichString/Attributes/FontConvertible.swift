7e82f243c9a0daa19db7b5a83caca171ca1e50bb
Danqing Liu <git@danqing.io>
2019-09-26 17:34:04 +0800

    Fix iOS 13 font regression (Times New Roman)

--- Sources/SwiftRichString/Attributes/FontConvertible.swift
+++ Sources/SwiftRichString/Attributes/FontConvertible.swift
@@ -51,7 +51,7 @@ public protocol FontConvertible {
 
 // MARK: - FontConvertible for UIFont/NSFont
 extension Font: FontConvertible {
-	
+
 	/// Return the same instance of the font with specified size.
 	///
 	/// - Parameter size: size of the font in points. If size is `nil`, `Font.systemFontSize` is used.
@@ -59,20 +59,20 @@ extension Font: FontConvertible {
 	public func font(size: CGFloat?) -> Font {
 		#if os(tvOS)
 		return Font(name: self.fontName, size: (size ?? TVOS_SYSTEMFONT_SIZE))!
-        #elseif os(iOS)
-        return Font(name: self.fontName, size: (size ?? Font.systemFontSize))!
 		#elseif os(watchOS)
 		return Font(name: self.fontName, size: (size ?? WATCHOS_SYSTEMFONT_SIZE))!
 		#elseif os(macOS)
-        return Font(descriptor: self.fontDescriptor, size: (size ?? Font.systemFontSize))!
+    return Font(descriptor: self.fontDescriptor, size: (size ?? Font.systemFontSize))!
+    #else
+    return Font(descriptor: self.fontDescriptor, size: (size ?? Font.systemFontSize))
 		#endif
 	}
-	
+
 }
 
 // MARK: - FontConvertible for String
 extension String: FontConvertible {
-	
+
 	/// Transform a string to a valid `UIFont`/`NSFont` instance.
 	/// String must contain a valid Postscript font's name.
 	///
@@ -87,5 +87,5 @@ extension String: FontConvertible {
 		return Font(name: self, size: (size ?? Font.systemFontSize))!
 		#endif
 	}
-	
+
 }
